e8d134b033ba24b2dbc6407be03d544c536d7c43
Maximiliano Briones <Maximilianonox@hotmail.com>
2026-09-14 10:12:26 -0300

    feat: nuevas funciones zona de carga

--- src/main/java/cl/speedfast/model/Repartidor.java
+++ src/main/java/cl/speedfast/model/Repartidor.java
@@ -1,21 +1,16 @@
 package cl.speedfast.model;
 
 import cl.speedfast.enums.EstadoPedido;
-import cl.speedfast.interf.Despachable;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Repartidor implements Runnable{
     private String nombre;
-    private List<Pedido> pedidosAsignados;
+    private ZonaDeCarga zonaDeCarga;
 
     public Repartidor(){}
 
-    public Repartidor (String nombre,ArrayList<Pedido> pedidosAsignados){
+    public Repartidor (String nombre,ZonaDeCarga zonaDeCarga){
         this.nombre = nombre;
-        this.pedidosAsignados = pedidosAsignados;
+        this.zonaDeCarga = zonaDeCarga;
     }
 
     public String getNombre() {
@@ -26,40 +21,56 @@ public class Repartidor implements Runnable{
         this.nombre = nombre;
     }
 
-    public List<Pedido> getPedidosAsignados() {
-        return pedidosAsignados;
+    public ZonaDeCarga getZonaDeCarga() {
+        return zonaDeCarga;
     }
 
-    public void setPedidosAsignados(List<Pedido> pedidosAsignados) {
-        this.pedidosAsignados = pedidosAsignados;
+    public void setZonaDeCarga(ZonaDeCarga zonaDeCarga) {
+        this.zonaDeCarga = zonaDeCarga;
     }
 
     public void run(){
-        try {
-            System.out.println("[INFO] " + nombre + " ha iniciado su turno con " + pedidosAsignados.size() + " pedido asignado.");
+        while (true) {
 
-            for (Pedido pedido : pedidosAsignados) {
-                if (pedido.getEstado() == EstadoPedido.CANCELADO) {
-                    System.out.println("[INFO] Pedido " + pedido.getIdPedido() + " esta cancelado. Se omite la entrega.");
-                    continue;
-                }
-                if (pedido.getRepartidorAsignado() == null || pedido.getRepartidorAsignado().isBlank()) {
-                    pedido.asignarRepartidor();
-                }
-                pedido.calcularTiempoEntrega();
-                System.out.println("[INFO] " + nombre + " ha iniciado el viaje para el pedido " + pedido.getIdPedido());
-                Thread.sleep(ThreadLocalRandom.current().nextInt(500, 1501));
-                System.out.println("[PROCESO] " + nombre + " esta en ruta a su destino " + pedido.getDireccionEntrega() + "....");
-                Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 3001));
-                pedido.despachar();
-                System.out.println("[EXITO] Pedido " + pedido.getIdPedido() + " entregado con exito por " + nombre);
+            Pedido pedido = zonaDeCarga.retirarPedido();
+
+            if (pedido == null) {
+                break;
             }
 
-            System.out.println("[FIN] " + nombre + " completo todas sus entregas.");
+            if (pedido.getEstado() == EstadoPedido.CANCELADO) {
+                System.out.println("[INFO] Pedido " + pedido.getIdPedido() + " esta cancelado. Se omite la entrega.");
+                continue;
+            }
 
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            System.out.println("[ERROR] La ruta de " + nombre + " fue interrumpida.");
+            pedido.setRepartidorAsignado(nombre);
+            pedido.setEstado(EstadoPedido.EN_REPARTO);
+
+            pedido.getHistorial().add(
+                    "Pedido #" + pedido.getIdPedido() + " paso a estado EN_REPARTO."
+            );
+
+            System.out.println("[INFO] " + nombre + " retiro el pedido #" + pedido.getIdPedido() + " y esta en reparto.");
+
+            try {
+
+                Thread.sleep(2000);
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+
+                System.out.println("[ERROR] La entrega de " + nombre + " fue interrumpida.");
+
+                return;
+            }
+
+            pedido.setEstado(EstadoPedido.ENTREGADO);
+
+            pedido.getHistorial().add(
+                    "Pedido #" + pedido.getIdPedido() + " entregado correctamente por " + nombre + "."
+            );
+
+            System.out.println("[EXITO] " + nombre + " entrego el pedido #" + pedido.getIdPedido() + " en " + pedido.getDireccionEntrega() + ".");
         }
     }
 }
